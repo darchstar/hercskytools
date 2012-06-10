5a84d09091642ad0f189d2800d910f29b57e6abb
Ameer Ghouse <aghouse1@gmail.com>
2012-06-09 20:09:44 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
-hercskytools
-============
+CM9 Build Instructions
+=======================
+```
+mkdir cm9
+cd cm9
+repo init -u git://github.com/CyanogenMod/android.git -b ics
+```
+
+Modify your `.repo/local_manifest.xml` as follows:
+
+```xml
+<?xml version="1.0" encoding="UTF-8"?>
+  <manifest>
+    <project name="TeamChopsticks/hercskytools" path="hercskytools" remote="github" revision="ics" />
+  </manifest>
+```
+
+```
+repo sync
+vendor/cm/get-prebuilts
+```
+
+Auto Apply Patches
+==================
+This script will remove any topic branches named auto, then apply all patches under topic branch auto.
+
+```
+hercskytools/cm9-skyrocket-apply.sh #if not working with the kernel & skyrocket
+hercskytools/cm9-skyrocket-3.0-apply.sh #if working with the kernel & skyrocket
+hercskytools/cm9-hercules-apply.sh #if working with hercules
+```
+
+Build
+=====
+
+```
+. build/envsetup.sh
+breakfast cm_skyrocket-userdebug #if building for skyrocket
+breakfast cm_hercules-userdebug #if building for hercules
+mka bacon
+```
---
+++ cm9-skyrocket-3.0-apply.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+unset SUCCESS
+on_exit() {
+  if [ -z "$SUCCESS" ]; then
+    echo "ERROR: $0 failed.  Please fix the above error."
+    exit 1
+  else
+    echo "SUCCESS: $0 has completed."
+    exit 0
+  fi
+}
+trap on_exit EXIT
+
+http_patch() {
+  PATCHNAME=$(basename $1)
+  curl -L -o $PATCHNAME -O -L $1
+  cat $PATCHNAME |patch -p1
+  rm $PATCHNAME
+}
+
+# Change directory verbose
+cdv() {
+  echo
+  echo "*****************************"
+  echo "Current Directory: $1"
+  echo "*****************************"
+  cd $BASEDIR/$1
+}
+
+# Change back to base directory
+cdb() {
+  cd $BASEDIR
+}
+
+# Sanity check
+if [ -d ../.repo ]; then
+  cd ..
+fi
+if [ ! -d .repo ]; then
+  echo "ERROR: Must run this script from the base of the repo."
+  SUCCESS=true
+  exit 255
+fi
+
+# Save Base Directory
+BASEDIR=$(pwd)
+
+# Abandon auto topic branch
+repo abandon auto
+set -e
+
+################ Apply Patches Below ####################
+
+repo start auto system/extra
+cdv system/extra
+echo "### ext4_utils: add BoardConfig define to suppress EMMC-corrupting wipe command http://review.cyanogenmod.com/#/c/17326/"
+git fetch http://review.cyanogenmod.com/CyanogenMod/android_system_extras refs/changes/26/17326/1 && git cherry-pick FETCH_HEAD
+cdb
+
+repo start auto device/samsung/skyrocket
+cdv device/samsung/skyrocket
+echo "### skyrocket: build kernel from source (DO NOT MERGE!) http://review.cyanogenmod.com/#/c/17430/"
+git fetch http://review.cyanogenmod.com/CyanogenMod/android_device_samsung_skyrocket refs/changes/30/17430/1 && git cherry-pick FETCH_HEAD
+cdb 
+
+##### SUCCESS ####
+SUCCESS=true
+exit 0
